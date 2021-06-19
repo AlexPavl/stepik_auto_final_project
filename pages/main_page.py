cc454cfc66b09f7a0ce220614c628234b9fde747
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-19 23:28:41 +0300

    Added functions and classes for login page

--- pages/main_page.py
+++ pages/main_page.py
@@ -1,10 +1,10 @@
 from .base_page import BasePage
 from selenium.webdriver.common.by import By
-from .locators import MainPageLocator
+from .locators import MainPageLocators
 
 class MainPage(BasePage):
     def go_to_login_page(self):
-        self.browser.find_element(*MainPageLocator.LOGIN_LINK).click()
+        self.browser.find_element(*MainPageLocators.LOGIN_LINK).click()
 
     def should_be_login_link(self):
-        assert self.is_element_present(*MainPageLocator.LOGIN_LINK), "Login link is not presented"
+        assert self.is_element_present(*MainPageLocators.LOGIN_LINK), "Login link is not presented"
