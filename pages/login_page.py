220a55ebe68ca3f432389e26929cbbdf36e12fe2
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-20 00:00:08 +0300

    Added login page checking

--- pages/login_page.py
+++ pages/login_page.py
@@ -8,7 +8,7 @@ class LoginPage(BasePage):
         self.should_be_register_form()
 
     def should_be_login_url(self):
-        assert 'login' in self.browser.current_url(), "Must be login in url"
+        assert 'login' in self.browser.current_url, "Must be login in url"
 
     def should_be_login_form(self):
         assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "LOG IN form is not presented"
