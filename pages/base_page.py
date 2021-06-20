f47f2105f9e6b566e1b8346fbb0bee2ed9d6eb58
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-20 20:55:56 +0300

    Few more tests for login from product page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,7 @@
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import TimeoutException
+from .locators import BasePageLocators
 
 class BasePage():
     def __init__(self, browser, url, timeout=10):
@@ -11,7 +12,7 @@ class BasePage():
     def open(self):
         self.browser.get(self.url)
 
-    def is_element_presented(self, how, what):
+    def is_element_present(self, how, what):
         try:
             self.browser.find_element(how, what)
         except:
@@ -31,3 +32,9 @@ class BasePage():
         except TimeoutException:
             return False
         return True
+
+    def go_to_login_page(self):
+        self.browser.find_element(*BasePageLocators.LOGIN_LINK).click()
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
--- test_product_page.py
+++ test_product_page.py
@@ -1,7 +1,7 @@
 from .pages.product_page import ProductPage
+from .pages.login_page import LoginPage
 import pytest
 
-@pytest.mark.skip
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -43,3 +43,21 @@ def test_message_disappeared_after_adding_product_to_basket(browser, link):
     product_page.open()
     product_page.add_product_to_basket()
     product_page.should_success_message_disappear()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+                                  "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/",
+                                  "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"])
+def test_guest_should_see_login_link_on_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.should_be_login_link()
+
+@pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/",
+                                  "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/",
+                                  "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"])
+def test_guest_can_go_to_login_page_from_product_page(browser, link):
+    page = ProductPage(browser, link)
+    page.open()
+    page.go_to_login_page()
+    login_page = LoginPage(browser, browser.current_url)
+    login_page.should_be_login_page()
