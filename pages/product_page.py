b145e78096fa89a24a4ab2bd3a46f16ff42ff903
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-20 17:51:58 +0300

    Parametrized product page test

--- pages/product_page.py
+++ pages/product_page.py
@@ -2,6 +2,8 @@
 from stepik_auto_final_project.pages.base_page import BasePage
 from .base_page import BasePage
 from .locators import ProductPageLocators
+import math
+from selenium.common.exceptions import NoAlertPresentException # в начале файла
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
@@ -15,3 +17,17 @@ class ProductPage(BasePage):
         product_price_message = self.browser.find_element(*ProductPageLocators.PRODUCT_PRICE_MESSAGE).text
         assert product_name == product_name_message, f"Name of added book ({product_name_message}) is not equal to one that you desired({product_name})"
         assert product_price == product_price_message, f"Price of basket({product_price_message}) is not equal to currently added product({product_price})"
+
+    def solve_quiz_and_get_code(self):
+        alert = self.browser.switch_to.alert
+        x = alert.text.split(" ")[2]
+        answer = str(math.log(abs((12 * math.sin(float(x))))))
+        alert.send_keys(answer)
+        alert.accept()
+        try:
+            alert = self.browser.switch_to.alert
+            alert_text = alert.text
+            print(f"Your code: {alert_text}")
+            alert.accept()
+        except NoAlertPresentException:
+            print("No second alert presented")
