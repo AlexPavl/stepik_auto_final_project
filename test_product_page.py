79aae05943bd30e7d016b9ac17b4521bb17de6de
Alexander Pavlov <saniok_92@inbox.ru>
2021-06-21 11:19:30 +0300

    Last few changes before code review

--- test_product_page.py
+++ test_product_page.py
@@ -8,7 +8,6 @@ books_links = ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_2
                 "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-shellcoders-handbook_209/",
                 "http://selenium1py.pythonanywhere.com/en-gb/catalogue/the-city-and-the-stars_95/"]
 
-@pytest.mark.new
 class TestUserAddToBasketFromProductPage():
     @pytest.fixture(autouse=True, scope="function")
     def setup(self, browser):
@@ -27,6 +26,7 @@ class TestUserAddToBasketFromProductPage():
         page.open()
         page.should_not_be_success_message()
 
+    @pytest.mark.need_review
     def test_user_can_add_product_to_basket(self, browser):
         link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
         page = ProductPage(browser=browser, url=link)
@@ -34,7 +34,7 @@ class TestUserAddToBasketFromProductPage():
         page.add_product_to_basket()
         page.is_added_to_basket()
 
-
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', ["http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer0",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer1",
                                   "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/?promo=offer2",
@@ -74,6 +74,7 @@ def test_guest_should_see_login_link_on_product_page(browser, link):
     page.open()
     page.should_be_login_link()
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', books_links)
 def test_guest_can_go_to_login_page_from_product_page(browser, link):
     page = ProductPage(browser, link)
@@ -82,6 +83,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser, link):
     login_page = LoginPage(browser, browser.current_url)
     login_page.should_be_login_page()
 
+@pytest.mark.need_review
 @pytest.mark.parametrize('link', books_links)
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser, link):
     page = ProductPage(browser=browser, url=link)
